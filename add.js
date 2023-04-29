87ae81286ae30bf5ee579c24b4008fb926716040
Chepudak <115375514+Chepudak@users.noreply.github.com>
2023-04-29 16:52:06 +0200

    Update add.js

--- add.js
+++ add.js
@@ -150,6 +150,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.firs_name} ${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.firs_name} 
+${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
